6e74cb4e2669a04c70e88bd982339836fe2cc84f
kaungsan-hub <la86ks42@gmail.com>
2020-12-17 23:40:45 +0630

    dec17

---
+++ php/sql/add.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ksweb</title>
+</head>
+<body>
+	<?php
+		if(isset($_POST["name"])&&isset($_POST["email"])&&isset($_POST["password"])){
+			$name=$_POST["name"];
+			$email=$_POST["email"];
+			$password=$_POST["password"];
+			$password=md5($password);
+
+			$connect =mysqli_connect("localhost","root","","php");
+			$result=mysqli_query($connect, "INSERT INTO `human` (`name`,`email`,`password`)VALUES('$name','$email','$password')");
+				mysqli_close($connect);
+		}
+	?>
+	<form action="add.php" method="POST" >
+		<table>
+			<tr>
+				<td>Name</td>
+				<td><input type="text" name="name" id="name"></td>
+				<td><span id="errname"></span></td>
+			</tr>
+			<tr>
+				<td>Email</td>
+				<td><input type="text" name="email" id="email"></td>
+				<td><span id="erremail"></span></td>
+			</tr>
+			<tr>
+				<td>Password</td>
+				<td><input type="password" name="password" id="password"></td>
+				<td><span id="errpass"></span></td>
+			</tr>
+			<tr> <td><input type="submit" value="OK" /></td> </tr>
+		</table>
+	</form>
+</body>
+</html>
---
+++ php/sql/show.php
@@ -0,0 +1,22 @@
+<?php 
+$connect = mysqli_connect("localhost", "root", "", "php");
+$result =  mysqli_query($connect, "SELECT * FROM human");
+   while ($f = mysqli_fetch_field($result)) {
+   	echo $f->name ."<br>"; 
+   }
+   echo "<hr>";
+   
+  while ($row= mysqli_fetch_array($result)) {
+   	echo $row["id"]."<br>";
+   	echo $row["name"]."<br>";
+   	echo $row["email"]."<br>";
+   	echo $row["password"]."<br>";
+   }
+
+   echo "<hr>";
+
+//	$row= mysqli_fetch_array($result);
+//	foreach ($row as $key=>$value) {
+//		echo "$value";
+//	}
+?>	
